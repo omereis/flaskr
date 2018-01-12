3282f2e37cf1a40e31bbd89cc9dbab0bbddb8de8
Omer Eisenberg <omereis@yahoo.com>
2018-01-12 15:44:41 -0500

    Basic working version

--- flasker/flaskr.py
+++ flasker/flaskr.py
@@ -41,9 +41,10 @@ def close_db(error):
 @app.route('/')
 def show_entries():
     db = get_db()
-    cur = db.execute('select title, entry_text from entries order by id desc')
-    entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)
+#    cur = db.execute('select title, entry_text from entries order by id desc')
+    cur = db.execute('select * from entries order by id desc')
+    blog_entries = cur.fetchall()
+    return render_template('show_entries.html', entries=blog_entries)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
@@ -51,7 +52,7 @@ def add_entry():
         abort(401)
     db = get_db()
     db.execute('insert into entries (title, entry_text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
+                 [request.form['title'], request.form['blog_text']])
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
